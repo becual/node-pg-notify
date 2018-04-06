a746c11a37232a414f9ec05678e1a4829a396620
gbahamondez <gbahamondezc@gmail.com>
2018-04-06 08:18:44 -0300

    Modifiyng test to add table customer
    - create table
    - listen
    - insert
    - drop table and trigger

--- src/__test__/subscribe.test.js
+++ src/__test__/subscribe.test.js
@@ -1,6 +1,6 @@
-/* global beforeAll jest expect */
+/* global beforeAll jest afterAll beforeAll */
 
-jest.setTimeout(30000);
+jest.setTimeout(10000);
 
 const { Client } = require('pg');
 const pgNotify = require('..');
@@ -11,14 +11,30 @@ const client = new Client({
 
 beforeAll(async () => {
     await client.connect();
+
+    await client.query(`CREATE TABLE IF NOT EXISTS customer (
+        id serial,
+        name text,
+        location text
+    )`);
 });
 
 
-test.only('Must subscribe to a table', async done => {
+test.only('Must subscribe to a table and fire insert event ', async done => {
     const pgEmiter = await pgNotify.subscribe(client, ['customer']);
 
-    pgEmiter.on('INSERT', data => {        
-        console.info('data at insert -> ', data);
+    pgEmiter.on('INSERT', data => {
+        console.info('data at insert event -> ', data);
         done();
     });
+
+    setTimeout(() => {
+        client.query('INSERT INTO customer(name, location) values (\'pepe\', \'santiago\')');
+    }, 3000);
+});
+
+
+afterAll(async () => {
+    await client.query('DROP TRIGGER notify_table_change on customer');
+    await client.query('DROP TABLE customer');
 });
